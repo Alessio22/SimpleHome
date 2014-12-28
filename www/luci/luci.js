71ad887bb01943b4985a46bf62492157abb719e9
Alessio Canestrelli <a.canestrelli@solvingteam.it>
2014-12-28 19:45:14 +0100

    fix luci

--- www/luci/luci.js
+++ www/luci/luci.js
@@ -11,20 +11,20 @@ angular.module('simpleHome.luci', ['ngRoute'])
 
 .controller('LuciCtrl', function($rootScope, $scope, $http) {
 	$scope.luci = [];
-	var req = {
-		method: 'GET', 
-		url: $rootScope.cfg.host+'user/luci_desc.xml', 
+	var reqDescrizioni = {
+		method: 'POST', 
+		url: $rootScope.cfg.host+'user/luci_desc.xml',
 		headers: {
 	    	'Authorization': 'Basic ' + btoa($rootScope.cfg.username+":"+$rootScope.cfg.password)
 	    }
 	};
-	$http(req).success(function(data, status, headers, config) {
+	$http(reqDescrizioni).success(function(data, status, headers, config) {
 		var response  = x2js.xml_str2json(data).response;
 
 		for(var i=0; i<46; i++) {
 			var desc = response["desc"+i];
 			if(desc!="") {
-				$scope.luci.push({"id":i, "desc": desc});
+				$scope.luci.push({"id":i, "desc": desc, "stato": 0});
 			}
 		}
 
@@ -33,22 +33,36 @@ angular.module('simpleHome.luci', ['ngRoute'])
 	  	alert('error');
 	});
 
-	$scope.change = function(id) {
-		req = {
-			method: 'GET', 
+	setInterval(function(){
+		var reqStato = {
+			method: 'POST', 
+			url: $rootScope.cfg.host+'user/luci.xml',
+			headers: {
+		    	'Authorization': 'Basic ' + btoa($rootScope.cfg.username+":"+$rootScope.cfg.password)
+		    }
+		};
+		$http(reqStato).success(function(data, status, headers, config) {
+			var response  = x2js.xml_str2json(data).response;
+			for(var i = 0;i < $scope.luci.length; i++){
+				$scope.luci[i].stato = response.stato.charAt(i) == 1 ? 'md-primary' : '' ;
+			}
+		});
+	}, 1000);
+
+	$scope.camboStato = function(id) {
+		var reqCambioStato = {
+			method: 'POST', 
 			url: $rootScope.cfg.host+'user/luci.cgi?luce='+id, 
 			headers: {
 		    	'Authorization': 'Basic ' + btoa($rootScope.cfg.username+":"+$rootScope.cfg.password)	
 		    }
 		};
-		$http(req).success(function(data, status, headers, config) {
-			alert('asd');
-			alert(data);
-			$scope.data = data;
-		  }).
-		  error(function(data, status, headers, config) {
-		  	alert('error');
-		  });
+		$http(reqCambioStato).success(function(data, status, headers, config) {
+			console.log(data);
+		}).
+		error(function(data, status, headers, config) {
+			alert('error');
+		});
 	};
 
 });
