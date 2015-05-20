5868867eefeac80092a1980283953f6fe2cca0d9
Alessio Canestrelli <a.canestrelli@solvingteam.it>
2015-05-20 09:15:37 +0200

    rimosso snapJs

--- www/app.js
+++ www/app.js
@@ -52,19 +52,3 @@ function onConfirm(button) {
         navigator.app.exitApp();
     }
 }
-
-// SnapJs
-var snapper = new Snap({
-  disable: 'right',
-  element: document.getElementById('content')
-});
-// var addEvent = function addEvent(element, eventName, func) {
-//   if (element.addEventListener) {
-//       return element.addEventListener(eventName, func, false);
-//     } else if (element.attachEvent) {
-//         return element.attachEvent("on" + eventName, func);
-//     }
-// };
-// addEvent(document.getElementById('open-left'), 'click', function(){
-//   snapper.open('left');
-// });
