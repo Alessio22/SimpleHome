998395a07a38bfeda8dfc36af71e43588edd37a5
Alessio Canestrelli <a.canestrelli@solvingteam.it>
2015-07-06 11:08:25 +0200

    nuova icona, status bar colorata e pulizia dei sorgenti

--- www/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('simpleHome.version', [
-  'simpleHome.version.interpolate-filter',
-  'simpleHome.version.version-directive'
-])
-
-.value('version', '0.1');
--- www/home/home.js
+++ www/home/home.js
@@ -15,10 +15,10 @@ angular.module('simpleHome.home', ['ngRoute'])
 	$scope.cfg = $rootScope.settings.cfg;
 
 	$scope.statoConfig = 0;
-	if($rootScope.settings.cfg.length > 0) { // se c'è la configurazione 
+	if($rootScope.settings.cfg.length > 0) { // se c'è la configurazione
 		$scope.statoConfig = 1;
 	}
-	if($rootScope.settings.profile) { // se è stato scelto un profilo 
+	if($rootScope.settings.profile) { // se è stato scelto un profilo
 		$scope.statoConfig = 2;
 	}
 
@@ -28,14 +28,14 @@ angular.module('simpleHome.home', ['ngRoute'])
 		var settings = { 'profile': $rootScope.settings.profile, 'cfg': [$rootScope.settings.cfg[0], $rootScope.settings.cfg[1]] }
 		localStorage.setItem("settings", JSON.stringify(settings));
 
-  		if(settings.profile == 0) {
-		    $rootScope.cfg.host = settings.cfg[0].host==undefined?'':settings.cfg[0].host;
+		if(settings.profile == 0) {
+	    $rootScope.cfg.host = settings.cfg[0].host==undefined?'':settings.cfg[0].host;
 			$rootScope.cfg.username = settings.cfg[0].username==undefined?'':settings.cfg[0].username;
-			$rootScope.cfg.password = settings.cfg[0].password==undefined?'':settings.cfg[0].password;  
+			$rootScope.cfg.password = settings.cfg[0].password==undefined?'':settings.cfg[0].password;
 		} else {
 		    $rootScope.cfg.host = settings.cfg[1].host==undefined?'':settings.cfg[1].host;
 			$rootScope.cfg.username = settings.cfg[1].username==undefined?'':settings.cfg[1].username;
-			$rootScope.cfg.password = settings.cfg[1].password==undefined?'':settings.cfg[1].password;  
+			$rootScope.cfg.password = settings.cfg[1].password==undefined?'':settings.cfg[1].password;
 		}
 
 		$scope.statoConfig = 2;
